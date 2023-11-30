36b40ed4d76655e097c2d1bbb7e2c83898a98fbf
geomontarroyos <gmontarroyosa@gmail.com>
2023-11-30 16:42:05 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -10,3 +10,36 @@ if ('serviceWorker' in navigator) {
     });
 }
     
+//configurando as constraintes do video stream
+var constraints = { video: {facingMode: "user"}, audio: false };
+//capturando os elementos em tela
+const cameraView = document.querySelector("#camera--view"),
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraTrigger = document.querySelector("#camera--trigger"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraVerso = document.querySelector("#camera--verso")
+  galeria = document.querySelector("#galeria")
+
+
+//Estabelecendo o acesso a camera e inicializando a visualização
+function cameraStart(){
+  navigator.mediaDevices
+   .getUserMedia(constraints)
+   .then(function (stream){
+    let track = stream.getTracks[0];
+    cameraView.srcObject = stream;
+ })
+  .catch(function (error){
+    console.error("Ocorreu um Erro.", error);
+ });
+}
+//função para tirar foto
+cameraTrigger.onclick = function () {
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
+};
+//carrega imagem de camera quando a janela carregar
+window.addEventListener("load", cameraStart, false);
